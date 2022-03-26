1ea9462e904040ea5cef5f9319caf82c00b97246
Kairyu <kairyu.bonvivant@gmail.com>
2022-03-26 13:58:21 +0900

    [Complete 03_php_algo]

--- 03_php_algo/algorithm02.php
+++ 03_php_algo/algorithm02.php
@@ -18,11 +18,35 @@
 // 100円玉で購入した場合、
 // 50円足りません。
 
-$yen = 10000;   // 購入金額
+$yen = 20037;   // 購入金額
 $product = 150; // 商品金額
 
 function calc($yen, $product) {
     // この関数内に処理を記述
+    $change = $yen - $product;
+    if ($change >= 0) {
+        echo "{$yen}円で{$product}円の商品を購入しました。<br>";
+        $yukichi = floor($change / 10000); //10000円札の枚数
+            $change -= $yukichi*10000; //10000をかけてお釣りから引く
+        $ichiyou = floor($change / 5000); //5000円札の枚数
+            $change -= $ichiyou*5000; //5000をかけてお釣りから引く
+        $hideyo = floor($change / 1000); //1000円札の枚数
+            $change -= $hideyo*1000; //1000をかけてお釣りから引く
+        $gohyaku = floor($change / 500); //500円玉の枚数
+            $change -= $gohyaku*500; //500をかけてお釣りから引く
+        $hyaku = floor($change / 100); //100円玉の枚数
+            $change -= $hyaku*100; //100をかけてお釣りから引く
+        $gojuu = floor($change / 50); //50円玉の枚数
+            $change -= $gojuu*50; //50をかけてお釣りから引く
+        $juu = floor($change / 10); //10円玉の枚数
+            $change -= $juu*10; //10をかけてお釣りから引く
+        $goen = floor($change / 5); //5円玉の枚数
+            $change -= $goen*5; //5をかけてお釣りから引く
+            //この時点で$changeは0~4の整数のどれかになり、それは1円玉の枚数に等しい
+        echo "お釣りは、10000円札が{$yukichi}枚、5000円札が{$ichiyou}枚、1000円札が{$hideyo}枚、500円玉が{$gohyaku}枚、100円玉が{$hyaku}枚、50円玉が{$gojuu}枚、10円玉が{$juu}枚、5円玉が{$goen}枚、1円玉が{$change}枚です。";
+    } else {
+        echo -$change."円足りません。";
+    }
 }
 ?>
 <!DOCTYPE html>
@@ -34,6 +58,9 @@ function calc($yen, $product) {
 <body>
     <section>
         <!-- ここに結果表示 -->
+        <?php
+            calc($yen, $product);
+        ?>
     </section>
 </body>
 </html>
